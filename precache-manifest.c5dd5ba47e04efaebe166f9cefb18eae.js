8109b3843a65244a02849b6da83b235c6d21650a
James Moriarty <jamespaulmoriarty@gmail.com>
2020-08-11 18:30:12 +1000

    Updates

---
+++ precache-manifest.c5dd5ba47e04efaebe166f9cefb18eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0a8dc4c05ef3bd92b12da42cc8d381",
+    "url": "/react-email-signature/index.html"
+  },
+  {
+    "revision": "fcf8c2dc3191c910c4d4",
+    "url": "/react-email-signature/static/css/main.7e3475f4.chunk.css"
+  },
+  {
+    "revision": "00b950fa359402d03f85",
+    "url": "/react-email-signature/static/js/2.f2482b66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-email-signature/static/js/2.f2482b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf8c2dc3191c910c4d4",
+    "url": "/react-email-signature/static/js/main.bbb6b185.chunk.js"
+  },
+  {
+    "revision": "c2e01960e911d030b5c2",
+    "url": "/react-email-signature/static/js/runtime-main.5dcaaf86.js"
+  }
+]);
